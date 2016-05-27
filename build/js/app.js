d8c1f13da95dd4d2bc3767fbaa68c406629e9106
arlen <burton.arlen.1990@gmail.com>
2016-05-27 12:02:17 -0700

    successfully call to a users repo page

--- build/js/app.js
+++ build/js/app.js
@@ -24,7 +24,7 @@ var apiKey = require('./../.env').apiKey;
 var repoArray =[];
 
 exports.apiRepo = function(username){
-  $.get('https://api.github.com/users/'+ username +'?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey + '&page=2&per_page=100&affiliation=owner&sort=created&direction=desc').then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
@@ -37,19 +37,16 @@ exports.apiUser = function(username){
 	$.get('https://api.github.com/users/' + username + '?access_token=' + apiKey)
 	.then(function(response){
 		$('#user').append(
-        '<img  src=' + response.avatar_url + '>' +
-        '<h2>Name: ' + response.name + '</h2>' +
-        '<h3>Email: ' + response.email + '</h3>' +
-        '<p>Repo Count: ' + response.public_repos + '</p>' +
-        '<p>Follower Count: ' + response.followers + '</p>'
+        '<img class="avatar" src=' + response.avatar_url + '>' +
+        '<h2>User Name: ' + username + '</h2>' +
+        '<h3>Location: ' + response.location + '</h3>' +
+        '<h3>Github URL: ' + response.html_url + '</h3>' +
+        '<h3>Repo Count: ' + response.public_repos + '</h3>' +
+        '<h3>Follower Count: ' + response.followers + '</h3>'
 		);
 	}).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
-
-
-// module.exports.apiUser = apiUser;
-// module.exports.apiRepo = apiRepo;
 
 },{"./../.env":1}]},{},[2]);
