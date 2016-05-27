295d6baa6ab164c0f580c771b13200d0b222fe29
arlen <burton.arlen.1990@gmail.com>
2016-05-27 15:02:11 -0700

    successufully pull data from repoCall

--- build/js/app.js
+++ build/js/app.js
@@ -21,11 +21,44 @@ $(document).ready(function() {
 
 },{"./../js/main.js":3}],3:[function(require,module,exports){
 var apiKey = require('./../.env').apiKey;
-var repoArray =[];
+
+function Repo(name, url, language, watcherCount, deepInfo) {
+  this.name = name;
+  this.url = url;
+  this.language = language;
+  this.watcherCount = watcherCount;
+  this.deepInfo = deepInfo;
+}
+var reObj = [];
+var repos = {
+  name: [],
+  url: [],
+  language: [],
+  watcherCount: [],
+  deepInfo: []
+};
 
 exports.apiRepo = function(username){
-  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey + '&page=2&per_page=100&affiliation=owner&sort=created&direction=desc').then(function(response){
-    console.log(response);
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey + '&page=2&per_page=10&affiliation=owner&sort=created&direction=desc').then(function(response){
+    // console.log(response);
+    response.forEach(function(repository){
+      reObj.push(repository);
+    });
+    console.log("reObj");
+    console.log(reObj[0].name);
+    console.log(reObj[0].clone_url);
+    console.log(reObj[0].language);
+    console.log(reObj[0].watchers_count);
+    console.log(reObj[0].url);
+    for(var i=0; i<=9; i++){
+      (repos.name).push(reObj[i].name);
+      (repos.url).push(reObj[i].clone_url);
+      (repos.language).push(reObj[i].language);
+      (repos.watcherCount).push(reObj[i].watchers_count);
+      (repos.deepInfo).push(reObj[i].url);
+    }
+    console.log("repos");
+    console.log(repos);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
@@ -36,6 +69,7 @@ exports.apiUser = function(username){
 
 	$.get('https://api.github.com/users/' + username + '?access_token=' + apiKey)
 	.then(function(response){
+    console.log(response);
 		$('#user').append(
         '<img class="avatar" src=' + response.avatar_url + '>' +
         '<h2>User Name: ' + username + '</h2>' +
@@ -46,6 +80,7 @@ exports.apiUser = function(username){
 		);
 	}).fail(function(error){
     console.log(error.responseJSON.message);
+    alert("something went wrong, check the developers panel");
   });
 };
 
--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
 var apiKey = require('./../.env').apiKey;
-var repoArray =[];
+
+function Repo(name, url, language, watcherCount, deepInfo) {
+  this.name = name;
+  this.url = url;
+  this.language = language;
+  this.watcherCount = watcherCount;
+  this.deepInfo = deepInfo;
+}
+var reObj = [];
+var repos = {
+  name: [],
+  url: [],
+  language: [],
+  watcherCount: [],
+  deepInfo: []
+};
 
 exports.apiRepo = function(username){
-  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey + '&page=2&per_page=100&affiliation=owner&sort=created&direction=desc').then(function(response){
-    console.log(response);
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey + '&page=2&per_page=10&affiliation=owner&sort=created&direction=desc').then(function(response){
+    // console.log(response);
+    response.forEach(function(repository){
+      reObj.push(repository);
+    });
+    console.log("reObj");
+    console.log(reObj[0].name);
+    console.log(reObj[0].clone_url);
+    console.log(reObj[0].language);
+    console.log(reObj[0].watchers_count);
+    console.log(reObj[0].url);
+    for(var i=0; i<=9; i++){
+      (repos.name).push(reObj[i].name);
+      (repos.url).push(reObj[i].clone_url);
+      (repos.language).push(reObj[i].language);
+      (repos.watcherCount).push(reObj[i].watchers_count);
+      (repos.deepInfo).push(reObj[i].url);
+    }
+    console.log("repos");
+    console.log(repos);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
@@ -14,6 +47,7 @@ exports.apiUser = function(username){
 
 	$.get('https://api.github.com/users/' + username + '?access_token=' + apiKey)
 	.then(function(response){
+    console.log(response);
 		$('#user').append(
         '<img class="avatar" src=' + response.avatar_url + '>' +
         '<h2>User Name: ' + username + '</h2>' +
@@ -24,5 +58,6 @@ exports.apiUser = function(username){
 		);
 	}).fail(function(error){
     console.log(error.responseJSON.message);
+    alert("something went wrong, check the developers panel");
   });
 };
